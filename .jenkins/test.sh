965ca57c66c1f2aeda7afed084f2a67fc9a281ec
Prasanna Jeevan Devanur Nagabhushan <jeevan@uber.com>
2017-08-03 17:35:06 -0700

    Added test cases

---
+++ .jenkins/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o pipefail
+set -ex
+
+PYENV_HOME=$WORKSPACE/.pyenv/
+
+# Delete previously built virtualenv
+if [ -d $PYENV_HOME ]; then
+	rm -rf $PYENV_HOME
+fi
+
+# Create virtualenv and install necessary packages
+virtualenv --no-site-packages $PYENV_HOME
+. $PYENV_HOME/bin/activate
+
+pip install --quiet tensorflow
+
+python $WORKSPACE/src/tf_helloworld.py
