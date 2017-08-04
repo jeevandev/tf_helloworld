32af2ce164273c7ecc032ff25fdfccdbd73c869f
Prasanna Jeevan Devanur Nagabhushan <jeevan@uber.com>
2017-08-03 17:31:18 -0700

    add test cases

--- src/tf_helloworld.py
+++ src/tf_helloworld.py
@@ -1,4 +1,10 @@
 import tensorflow as tf
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
-print sess.run(hello)
+
+
+def tf_hello():
+    hello = tf.constant('Hello, TensorFlow!')
+    sess = tf.Session()
+    print sess.run(hello)
+
+if __name__ == "__main__":
+    tf_hello()
